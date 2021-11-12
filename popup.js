893a78dd6fae19a7319cdafbd6bafc4967eba3b1
maxius89 <maxius89@gmail.com>
2021-11-12 13:59:35 +0100

    Init commit

---
+++ popup.js
@@ -0,0 +1,36 @@
+// Initialize button with users's prefered color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  let url = tab.url;
+  chrome.storage.sync.set({ url });
+  chrome.storage.sync.set({ tab });
+
+  chrome.tabs.update(undefined, { url: url + "wheeee" });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+
+  chrome.storage.sync.get("url", ({ url }) => {
+    console.log(url);
+  });
+
+  
+}
