82754cfa3dca9f42ab78c4b6fa38675c6838417c
maxius89 <maxius89@gmail.com>
2021-11-13 23:19:05 +0100

    Cleaned up code from tutorial example

--- background.js
+++ background.js
@@ -1,12 +1,37 @@
-let color = '#3aa757';
 let toBeReplaced = "hu";
 let toBeReplacedWith = "com";
 
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-
   chrome.storage.sync.set({ toBeReplaced });
   chrome.storage.sync.set({ toBeReplacedWith });
 });
+
+chrome.action.onClicked.addListener(async () => {
+  let tab = await getCurrentTab();
+
+  chrome.storage.sync.set({ url: tab.url });
+
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: createNewUrl
+  });
+
+  chrome.storage.sync.get("newUrl", (data) => {
+    chrome.tabs.update(undefined, { url: data.newUrl});
+  });
+
+});
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+function createNewUrl() {
+  chrome.storage.sync.get(["toBeReplaced", "toBeReplacedWith", "url"], (data) => {
+    let newUrl = data.url.replace(data.toBeReplaced, data.toBeReplacedWith);
+    
+    chrome.storage.sync.set({ newUrl });
+  });
+}
